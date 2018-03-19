3ee17a98c81dc9b22abb2a29c3bbc493f4ee488a
sunfeng <sunfeng0712@163.com>
2018-03-19 18:02:39 +0800

    add var_dump for dd methods

--- helpers/BaseExtend.php
+++ helpers/BaseExtend.php
@@ -5,15 +5,27 @@
  * Date: 2018/3/19
  * Time: 下午5:48
  */
-namespace yii2extend\med\helpers;
+namespace yii2extend\mod\helpers;
 
 use yii;
 
 /**
  * Class BaseExtend
- * @package yii2extend\med\helpers
+ * @package yii2extend\mod\helpers
  */
 class BaseExtend
 {
-
+    /**
+     * common debug for php
+     * @param $param
+     * @return array
+     */
+    public static function dd($param)
+    {
+        foreach ($param as $p)  {
+            \yii\helpers\VarDumper::dump($p, 10, true);
+            echo '<pre>';
+        }
+        exit(1);
+    }
 }
