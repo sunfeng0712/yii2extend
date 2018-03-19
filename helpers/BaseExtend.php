34bbc450770eb2c34bb467dd53a21fb44d525d6b
sunfeng <sunfeng0712@163.com>
2018-03-19 17:52:54 +0800

    add .gitignore

---
+++ helpers/BaseExtend.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2018/3/19
+ * Time: 下午5:48
+ */
+namespace yii2extend\med\helpers;
+
+use yii;
+
+/**
+ * Class BaseExtend
+ * @package yii2extend\med\helpers
+ */
+class BaseExtend
+{
+
+}
